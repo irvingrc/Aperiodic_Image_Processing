1f99ac3cfd0ab91847caa5178846ac92bc289d2c
irvingrc <irving.rc@asu.edu>
2024-05-19 14:06:02 -0700

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 12:56:45 2023
+
+@author: nikkivanhandel
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 13:58:19 2022
+
+@author: nikkivanhandel
+
+This script is mostly just functions that I use in the analysis script. 
+Functions are used on a per image basis
+
+"""
+
+import cv2
+import numpy as np
+from skimage import measure
+from scipy.ndimage import convolve, label
+from scipy.ndimage import distance_transform_edt as dt_edt
+from skimage.morphology import skeletonize, thin
+
+def logical_minus(keep, *arrays):
+    n = len(arrays)
+    diff = keep.astype(int) * n
+    for arr in arrays:
+        diff -= arr.astype(int)
+    
+    return diff==n
+
+def process(p): 
+    '''
+    Performs preprocessing for lattice images. Intended for Keyence/Blue light scans. 
+
+    Parameters
+    ----------
+    p : str
+        path to file to read and process.
+
+    Returns
+    -------
+    closing : bool array
+        Binarized lattice image.
+
+    '''
+    kernel = np.ones((5,5), np.uint8)
+    gray = cv2.imread(p, cv2.IMREAD_GRAYSCALE)
+    blur = cv2.GaussianBlur(gray, (13,13),0)
+    _, image = cv2.threshold(blur, np.mean(blur), 1, cv2.THRESH_BINARY)
+    if np.sum(image) > image.size/2:
+        image = np.logical_not(image)
+    closing = cv2.morphologyEx(image.astype(np.uint8), cv2.MORPH_CLOSE, kernel)
+    return closing.astype(bool)
+
+def get_nodes(arr, c=1): 
+    idx = np.argwhere(arr)
+    top, left =  idx.min(axis=0)
+    bottom, right = idx.max(axis=0)
+    starts = [(left, top), (right, top),
+              (left, bottom), (right, bottom)]
+    k = 50
+    ends = [(left+k, top+k), (right-k, top+k),
+            (left+k, bottom-k), (right-k, bottom-k)]
+    for line in range(4):
+        arr = cv2.line(arr, starts[line], ends[line],
+                           c, 1) 
+        
+    kernel = np.ones((3,3))
+    kernel[1,1] = 9 # this means that the middle pixel is most weighted
+    nodes = convolve(arr, kernel, mode='constant') >= 12 
+    nodes = cv2.dilate(nodes.astype(np.uint8), np.ones((3,3)))
+    return nodes
+
+def segmentize(image):
+    '''
+    Accepts a 2D lattice and returns the nodes and segments of the lattice.  
+    Parameters
+    ----------
+    image : bool type image array
+        Preprocessed image of a lattice (from process fxn).
+
+    Returns
+    -------
+    image : bool type array
+        Standardizes the array s.t. the positive space is 1 and negative space is 0.
+    nodes : bool type array
+        image with all skeleton node points = 1.
+    segments : bool type array
+        skeleton minus the nodes - completely segmented struts of thickness 1.
+
+    '''
+    sk = skeletonize(image)
+    nodes = get_nodes(sk.astype(np.uint8))
+    sk = thin(sk)
+    seg = (((sk+1) - nodes)>1).astype(np.uint8)
+    return nodes.astype(bool), seg.astype(bool), sk.astype(bool)
+
+
+def expand_nodes(bit_nodes, k=15):
+    '''
+    Isolates the full size struts from the binary lattice using the information from segmentize.  
+
+    Parameters
+    ----------
+    cell_im : bool type image array
+        Binary image of the lattice where the lattice is 1 and background is 0.
+    bit_nodes : bool type image array
+        Binary image of lattice nodes (from segmentize). Nodes are 1px in size
+    k : int
+        dilation size (based on thickness)
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    '''
+    # Removes the nodes from the binary image to isolate struts
+    k = round(k)
+    kern = np.ones((k,k))
+    nodes = cv2.dilate(bit_nodes.astype(np.uint8), kern)
+    nodes = cv2.morphologyEx(nodes, cv2.MORPH_CLOSE, kern)
+    ell_kern =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(k,k))
+    dil_nodes = cv2.dilate(nodes.astype(np.uint8), ell_kern)
+    # Find Corners using Harris 
+    #dst = cv2.cornerHarris(cell_im.astype(np.float32),k,3,0.01)
+    #dst = cv2.dilate(dst, np.ones((k,k)))
+    #corners = dst > 0.001
+    return dil_nodes
+
+def connectivity(segments, nodes, dil_nodes, return_map=False):
+    '''
+    Determine how many struts meet at a given node
+
+    Parameters
+    ----------
+    segments : TYPE
+        bool array where all strut segments are .
+    nodes : bool array
+        Bool array where all nodes are marked as 1.
+    dil_nodes : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    node_connectivity : TYPE
+        DESCRIPTION.
+
+    '''
+    # How many branches per region? 
+    connect_map = dil_nodes.copy()   # Unsuppress for 
+    _, node_labels = cv2.connectedComponents(dil_nodes.astype(np.uint8))
+    node_props = measure.regionprops(node_labels, cache=True)
+    c = []
+    for r in node_props:
+        (x1, y1, x2, y2) = r.bbox
+        sgno, _ = cv2.connectedComponents(segments[x1:x2, y1:y2].astype(np.uint8))
+        nono, _ = cv2.connectedComponents(nodes[x1:x2, y1:y2].astype(np.uint8))
+        total = sgno - nono + 1
+        c.append(total)
+        connect_map[node_labels==r.label] = total
+    node_connectivity = np.mean(c)
+    return node_connectivity, connect_map, c
+        
+    
+def thickness(im, nodes, sk):
+    dist_edt = dt_edt(im)
+    strut_thick = 2*np.mean(dist_edt[sk])
+    node_thick = np.mean(dist_edt[nodes])
+    return dist_edt, strut_thick, node_thick
+
+
+
+
+def color_by(labelled, reg, feat='area', func=None, amin=0):
+    '''
+    Replaces labels with desired feature values
+
+    Parameters
+    ----------
+    labelled : int32 array
+        result of measure.label.
+    regions : pandas DataFrame
+        DataFrame from regionprops obj.
+    feat : TYPE, optional
+        DESCRIPTION. The default is 'area'.
+
+    Returns
+    -----
+    colored : TYPE
+        DESCRIPTION.
+
+    '''
+    colored = np.zeros_like(labelled)
+    feat_list=[]
+    if func is not None:
+        for r in reg:
+            coords = r['coords']
+            f = func(r)
+            colored[tuple(zip(*coords))] = f
+            feat_list.append(f)
+    else: # Use feature 
+        for r in range(len(reg)):
+            coords, f = reg.loc[r, 'coords'], reg.loc[r, feat]
+            colored[tuple(zip(*coords))] = f
+            feat_list.append(f)        
+
+    return colored, feat_list
+
+def length_thick(im, dt, d=15, listed=True):
+    _, label_im = cv2.connectedComponents(im.astype(np.uint8))
+    dil_im = cv2.dilate(label_im.astype(np.uint16), np.ones((d,d)))
+    ncc = np.amax(label_im)
+    z = np.zeros_like(im.astype(np.float32))
+    if listed:
+        feats = []
+        for comp in range(1, ncc+1):
+            mask = label_im==comp
+            length = measure.regionprops(1*mask)[0].major_axis_length
+            avg_thick = np.mean(dt[mask])
+            feats.append(length/avg_thick)
+        return feats
+    else:
+        for comp in range(1, ncc+1):
+            mask = label_im==comp
+            length = measure.regionprops(1*mask)[0].major_axis_length
+            avg_thick = np.mean(dt[mask])
+            z[dil_im==comp] = length/avg_thick
+        return z
+
+def shape_analyze(im, bg=0, minval=0, crit='area', table=False, xprops=[]):
+    '''
+    processing for a labelled connected components image
+
+    Parameters
+    ----------
+    label_im : int32 array
+        result of measure.label.
+    bg : bool, either 0 or 1
+        What value to treat as the background. The default is 0.
+    amin : int, optional
+        Optionally filter the minimum size of components. The default is 0.
+    xprops: list of str, optional
+        Additional properties besides those assumed (see props)
+    
+    Returns
+    -------
+    label_im: int32 array
+        label image of connected components.
+    tab: pandas DataFrame
+        region properties (adjust based on props below)
+
+    '''
+    props = ['label', 'area', 'orientation', 
+             'minor_axis_length', 'major_axis_length'] + xprops
+    
+    if bg != 0: 
+        _, label_im = cv2.connectedComponents(np.logical_not(im).astype(np.uint8))
+    else:
+        _, label_im = cv2.connectedComponents(im.astype(np.uint8))
+
+    if bg != 0: #if characterizing negative space 
+        label_im[np.where(label_im>=1)] -= 1 # Remove bg
+    regions = measure.regionprops(label_im, cache=False)
+    
+    if minval > 0:
+        for p in regions:
+            if p[crit] < minval:
+                label_im[label_im==p.label] = 0
+                label_im[label_im>p.label] -= 1
+      
+    if table:
+        propout = measure.regionprops_table(label_im, properties=props)
+                                          #properties = props)
+    else:                                   
+        propout = measure.regionprops(label_im, cache=False)
+    #tab = pd.DataFrame(tab)
+    return label_im, propout
+    
---
+++ visualize.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 13:47:34 2023
+
+@author: nikkivanhandel
+"""
+
+import matplotlib.pyplot as plt
+from math  import sin, cos
+import pandas as pd
+from preprocess import *
+from matplotlib.colors import ListedColormap
+from seaborn import heatmap
+
+def multi_imshow(*arrays, layout=None, cbar=False, cmap='viridis'):
+    num_arrays = len(arrays)
+    
+    if layout is None:
+        layout = (1, num_arrays)
+    else:
+        layout = (layout[0], layout[1])
+    
+    fig, axes = plt.subplots(layout[0], layout[1])
+    
+    for i, ax in enumerate(axes.flat):
+        if i < num_arrays:
+            img = ax.imshow(arrays[i], cmap=cmap)
+            ax.axis('off')
+            
+    if cbar:
+        fig.colorbar(img, ax=ax)
+        
+    plt.tight_layout()
+    
+    return fig
+
+
+def color_struts(colored, zmask=None, struts=True, isax=None, d=15, bounds=None, cellmask=None):
+
+    if isax is None:
+        fig, ax = plt.subplots()
+    else:
+        ax = isax
+        
+    if struts:
+        colored = cv2.dilate(colored.astype(np.float32), np.ones((d,d)))
+    
+    if zmask is None:   
+        mask = colored != 0 # Mask zero terms 
+    else:
+        mask = cv2.dilate(zmask.astype(np.uint8), np.ones((d,d)))
+        
+    if bounds is None:
+        vmin, vmax = np.min(colored), np.max(colored)
+    else:
+        vmin, vmax = bounds
+            
+    if isax is None:
+        im1 =ax.imshow(colored, cmap='coolwarm')
+        plt.colorbar(im1)
+    else:
+        im1= ax.imshow(colored, cmap= 'coolwarm', vmin=vmin, vmax=vmax)
+        
+    if cellmask is None:
+        ax.imshow(np.ma.masked_array(colored, mask), cmap='binary_r' )
+    else: 
+        ax.imshow(np.ma.masked_array(cellmask, mask), cmap='binary_r' )
+        
+    ax.set_yticks([]), ax.set_xticks([])
+    
+    if isax is None:
+        return fig
+    else:
+        return ax, im1
+
+def color_by_thickness(im, segments, minval=50):
+    dt = dt_edt(im)
+    label = measure.label(segments)
+    props = measure.regionprops(label, cache=False)
+    colored = np.zeros_like(im).astype(np.float32)
+    for p in props:
+        if p.area > minval:
+            coords = p['coords']
+            tr = tuple(zip(*coords))
+            colored[tr] = np.mean(dt[tr])
+    return colored
+
+def pixel_represent(arr,annot=True, mask=None):
+    fig, ax = plt.subplots()
+    heat = heatmap(arr, cmap='viridis', ax=ax,
+                   xticklabels=[], yticklabels=[], 
+                   cbar=False, linewidths=1,
+                   annot=annot)
+    if mask is not None and annot:
+        for text, show_annot in zip(ax.texts, mask.ravel()):
+            text.set_visible(show_annot)
+            
+
+def comp_sk(skim, node_im, dil, bounds=[0, 600, 0, 600]):
+    fig, axes = plt.subplots(1,3)
+    x1, x2, y1, y2 = bounds
+    vers = [skim, node_im, logical_minus(skim, dil)]
+    kern = np.ones((15, 15))
+
+    for v in range(3):
+        vers[v] = cv2.dilate(((vers[v])[x1:x2, y1:y2]).astype(np.uint8), kern)
+    vers[1] = vers[0] - vers[1]
+    til = ['skeleton', 'nodeless', 'segments']
+    for i, ax in enumerate(axes.reshape(-1)):
+        ax.axis('off') 
+        #ax.set_title(til[i])
+        ax.imshow(vers[i])
+    
+    plt.savefig('test.png', dpi=500, bbox_inches='tight')
+        
+
+def compare_thicknesses(cell_im, skim, node_im):
+    thick1 = thickness(cell_im, skim)
+    dist_edt = dt_edt(cell_im)
+    dil_nodes = expand_nodes(node_im, k=round(thick1))
+    thick2 = np.mean(dist_edt[segment_im])
+    thick3 = np.mean(dist_edt[logical_minus(skim, dil_nodes)])
+    return [thick1, thick2, thick3]
+
+    df  = pd.DataFrame(thicknesses, columns=['specimen', 'skeleton', 'sk-nodeless', 'segments'] )
+    df = 2*df.set_index('specimen')
+    return df
+
+def discrete_cbar(colormaps='hsv'):
+    #ticks = np.arange(np.amin(image), np.amax(image+1))
+    ticks = np.arange(2, 10)
+    colors = []
+    cmap = plt.get_cmap(colormaps)
+    colors.extend(cmap(np.linspace(0, 1, len(ticks))))
+    newcmap = ListedColormap(colors)
+    return newcmap, ticks
+
+def node_connectivity_map(cell_im, c, ax=None,bar=True):
+    if ax is None:
+        fig, ax = plt.subplots()
+        
+    im1 = ax.imshow(cell_im, cmap='binary_r')
+    ax.axis('off')
+    ax.patch.set_edgecolor('black')  
+    ax.patch.set_linewidth('1')  
+
+    cmask = np.ma.masked_where(c==0, c)
+    
+    custom, ticks = discrete_cbar()#cmask)
+    
+    im2 = ax.imshow(cmask, cmap=custom, vmin=2, vmax=8)
+    if bar:
+        cbar = ax.figure.colorbar(im2, ax=ax, ticks=ticks)
+        cbar.ax.set_yticklabels(ticks)
+    if ax is None:
+        return fig
+    else:
+        return ax, im2
+
+
+def see_nodes(cells, dnodes, nodes, segments):
+    white = logical_minus(cells, dnodes)
+    red = logical_minus(cells, segments)
+    yellow = logical_minus(cells, nodes)
+    stack = np.dstack((white, red, yellow))
+    return stack
+    #cv2.imwrite(push+'nodes_'+name+'.png', 255*stack)
+
+def draw_min_maj(image):
+    # Visualize 
+    label_img = measure.label(image)
+    regions = measure.regionprops(label_img)
+    # Remove BG if necessary
+    if label_img[0,0] != 0:
+        label_img[label_img==label_img[0,0]] = 0
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap=plt.cm.gray)
+    
+    for p in regions:
+        if p.area > 1000:
+            y0, x0 = p.centroid
+            pmax, pmin =0.5* p.axis_major_length, 0.5*p.axis_minor_length
+            orient = p.orientation
+            x1 = x0 + cos(orient) * pmin
+            y1 = y0 - sin(orient) * pmin
+            x2 = x0 - sin(orient) * pmax
+            y2 = y0 - cos(orient) * pmax
+        
+            ax.plot((x0, x1), (y0, y1), '-r', linewidth=1)
+            ax.plot((x0, x2), (y0, y2), '-b', linewidth=1)
+            ax.plot(x0, y0, '.g', markersize=1)
+    plt.axis('off')
+    return fig
+
+def plot_hist(skel, seg_reg, space_reg, path, filename):
+    segmap, spmap = np.zeros_like(skel*1), np.zeros_like(skel*1) 
+    seglen = []
+    for seg in seg_reg:
+        coord, dia = seg.coords, seg.major_axis_length
+        segmap[tuple(zip(*coord))] = dia
+        seglen.append(dia)
+    
+    spsz=[]
+    for s in space_reg:
+        coord, dia = s.coords, s.area
+        spmap[tuple(zip(*coord))] = dia
+        spsz.append(dia)
+    spsz = sorted(spsz)
+    
+    plt.figure()
+    plt.imshow(spmap, cmap='hot', vmax=spsz[-2])
+    plt.axis("off")
+    
+    plt.savefig(path + "cellA-"+ filename + ".png", dpi=1000, bbox_inches="tight")
+    
+    fig, ax = plt.subplots(1,2)
+    spplt = ax[0].imshow(spmap, cmap='hot', vmax=spsz[-2])
+    plt.colorbar(spplt,ax =  ax[0])
+    
+    segplt = ax[1].imshow(segmap, cmap='hot', vmin=1)
+    plt.colorbar(segplt,ax = ax[1])
+    
+    ax[0].axis('off')
+    ax[1].axis('off')
+    
+    ax[0].title.set_text('Cell Area')
+    ax[1].title.set_text('Max Axis Length')
+    
+    plt.savefig(path + "map-"+ filename + ".png", dpi=1000, bbox_inches="tight")
+    
+    fig, ax = plt.subplots(1,2)
+    
+    ax[0].hist(seglen, range=(1, max(seglen)))
+    ax[1].hist(spsz, range=(spsz[0], spsz[-2]))
+    ax[0].title.set_text('Cell Area')
+    ax[1].title.set_text('Max Axis Length')   
+    
+def quickplot(im, cax=None):
+    if cax is None:
+        fig, ax = plt.subplots()
+    ax.imshow(im)
+    ax.axis('off')
+    
